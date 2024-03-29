a5374373cf26de33fd850bf1add1cff01c959cf6
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-29 17:29:31 -0500

    Add boilerplate files

---
+++ script.js
@@ -0,0 +1,35 @@
+// add
+// subtract
+// multiply
+// divide
+
+// let leftOperand
+// let operator
+// let rightOperand
+
+// function operate(leftOperand, operator, rightOperand) {
+
+// }
+
+function add(num1, num2) {
+  return num1 + num2;
+};
+
+function subtract(num1, num2) {
+  return num1 - num2;
+};
+
+function multiply(num1, num2) {
+  return num1 * num2;
+};
+
+function divide(leftOperand, rightOperand) {
+  return leftOperand / rightOperand;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide
+};
